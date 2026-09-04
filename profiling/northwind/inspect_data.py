a916ca5d1dde2cca0462f55b3075219f5e27c084
deniskibichiy <deniskibichiy@outlook.com>
2026-09-04 20:20:17 +0300

    document data profiling using pandas

---
+++ profiling/northwind/inspect_data.py
@@ -0,0 +1,31 @@
+#import pandas as pd
+import pandas as pd
+#customers
+customers = pd.read_csv(
+    "customers.csv",
+    sep="|",
+    header = "infer"
+)
+
+#orders
+
+orders = pd.read_csv(
+    "orders.csv",
+    sep = "|",
+    header = "infer"
+)
+
+# Transforming columns to remain with only relevant columns
+
+## Find out how many null values we have in each column
+orders.isnull().sum()
+
+# Since ShippedRegion contains 507 null values, this column deserves dropping
+# Save only the columns you want to a new file
+orders.drop(columns=["ShipAddress", "ShipPostalCode"]).to_csv(
+    "orders_clean.csv", index=False
+)
+
+
+
+
